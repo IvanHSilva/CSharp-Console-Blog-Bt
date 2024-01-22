8b93aafb4fa5ca124e6e2d369554015ee7ae14f4
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-01-22 19:50:11 -0300

    Read Users

--- Program.cs
+++ Program.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
 
 namespace Blog
 {
     class Program
     {
+        private const string connectionString =
+            "Data Source=.\\SQLSERVER;Database=Blog;Integrated Security=True;Encrypt=False;";
+
         static void Main(string[] args)
         {
+            ReadUsers();
+        }
+
+        public static void ReadUsers()
+        {
+            Console.Clear();
+            Console.WriteLine($"Usuários:");
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var users = connection.GetAll<User>();
+
+                foreach (var user in users)
+                {
+                    Console.WriteLine($"{user.Name}");
+                }
+            }
+
             Console.WriteLine();
         }
     }

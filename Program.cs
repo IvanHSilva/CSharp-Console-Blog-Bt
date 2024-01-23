2d890296c7085ccf2f340d20980ef7a586d1ad03
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-01-23 17:19:12 -0300

    Insert, Update and Delete User

--- Program.cs
+++ Program.cs
@@ -11,12 +11,40 @@ namespace Blog
 
         static void Main(string[] args)
         {
+            //ReadUsers();
+            //SelectUser(1);
+
+            // User user = new()
+            // {
+            //     Name = "Davi Henriques",
+            //     Email = "davilealhenriques@gmail.com",
+            //     PasswordHash = "0145451321654ASGJUYTECSAX",
+            //     Bio = "Estudante",
+            //     Image = "https://",
+            //     Slug = "davi-henriques"
+            // };
+            // //InsertUser(user);
+
+            User user = new()
+            {
+                Id = 7,
+                Name = "Angela Henriques",
+                Email = "angelahenriquessilva@gmail.com",
+                PasswordHash = "123545456654ASGJUYTECSAX",
+                Bio = "Dona de Casa",
+                Image = "https://",
+                Slug = "angela-henriques"
+            };
+
+            //UpdatetUser(user);
+
+            DeleteUser(7);
             ReadUsers();
         }
 
         public static void ReadUsers()
         {
-            Console.Clear();
+            Console.WriteLine();
             Console.WriteLine($"Usuários:");
 
             using (var connection = new SqlConnection(connectionString))
@@ -25,8 +53,70 @@ namespace Blog
 
                 foreach (var user in users)
                 {
-                    Console.WriteLine($"{user.Name}");
+                    Console.WriteLine($"{user.Name} - Id {user.Id}");
                 }
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void SelectUser(int id)
+        {
+            Console.Clear();
+            Console.WriteLine($"Usuário:");
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var user = connection.Get<User>(id);
+
+                Console.WriteLine($"{user.Name}");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void InsertUser(User user)
+        {
+            Console.Clear();
+            Console.WriteLine($"Novo Usuário:");
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Insert(user);
+
+                Console.WriteLine($"{user.Name}");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void UpdatetUser(User user)
+        {
+            Console.Clear();
+            Console.WriteLine($"Usuário Atualizado:");
+            SelectUser(user.Id);
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Update(user);
+
+                Console.WriteLine($"{user.Name}");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void DeleteUser(int id)
+        {
+            Console.Clear();
+            Console.WriteLine($"Usuário Excluído:");
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                User user = connection.Get<User>(id);
+                connection.Delete(user);
+
+                Console.WriteLine($"{user.Name}");
             }
 
             Console.WriteLine();

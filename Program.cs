fc578272be7edb22b81a253bcffee6e12828e688
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-01-25 20:56:34 -0300

    Respositories optimization

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using Blog.Models;
 using Blog.Repositories;
-using Blog.RoleRepositories;
-using Dapper.Contrib.Extensions;
 using Microsoft.Data.SqlClient;
 
 namespace Blog
@@ -50,88 +48,38 @@ namespace Blog
             Console.WriteLine($"Regras:");
             ReadRoles(connection);
             Console.WriteLine();
+            Console.WriteLine($"Tags:");
+            ReadTags(connection);
+            Console.WriteLine();
 
             connection.Close();
         }
 
         public static void ReadUsers(SqlConnection connection)
         {
-            var repository = new UserRepository(connection);
-            var users = repository.SelectAll();
+            var repository = new Repository<User>(connection);
+            var items = repository.SelectAll();
 
-            foreach (var user in users)
-                Console.WriteLine($"{user.Name} - Id {user.Id}");
+            foreach (var item in items)
+                Console.WriteLine($"{item.Name} - Id {item.Id}");
         }
 
         public static void ReadRoles(SqlConnection connection)
         {
-            var repository = new RoleRepository(connection);
-            var roles = repository.SelectAll();
+            var repository = new Repository<Role>(connection);
+            var items = repository.SelectAll();
 
-            foreach (var role in roles)
-                Console.WriteLine($"{role.Name} - Id {role.Id}");
+            foreach (var item in items)
+                Console.WriteLine($"{item.Name} - Id {item.Id}");
         }
 
-        public static void SelectUser(int id)
+        public static void ReadTags(SqlConnection connection)
         {
-            Console.Clear();
-            Console.WriteLine($"Usuário:");
+            var repository = new Repository<Tag>(connection);
+            var items = repository.SelectAll();
 
-            using (var connection = new SqlConnection(connectionString))
-            {
-                var user = connection.Get<User>(id);
-
-                Console.WriteLine($"{user.Name}");
-            }
-
-            Console.WriteLine();
-        }
-
-        public static void InsertUser(User user)
-        {
-            Console.Clear();
-            Console.WriteLine($"Novo Usuário:");
-
-            using (var connection = new SqlConnection(connectionString))
-            {
-                connection.Insert(user);
-
-                Console.WriteLine($"{user.Name}");
-            }
-
-            Console.WriteLine();
-        }
-
-        public static void UpdatetUser(User user)
-        {
-            Console.Clear();
-            Console.WriteLine($"Usuário Atualizado:");
-            SelectUser(user.Id);
-
-            using (var connection = new SqlConnection(connectionString))
-            {
-                connection.Update(user);
-
-                Console.WriteLine($"{user.Name}");
-            }
-
-            Console.WriteLine();
-        }
-
-        public static void DeleteUser(int id)
-        {
-            Console.Clear();
-            Console.WriteLine($"Usuário Excluído:");
-
-            using (var connection = new SqlConnection(connectionString))
-            {
-                User user = connection.Get<User>(id);
-                connection.Delete(user);
-
-                Console.WriteLine($"{user.Name}");
-            }
-
-            Console.WriteLine();
+            foreach (var item in items)
+                Console.WriteLine($"{item.Name} - Id {item.Id}");
         }
     }
 }
